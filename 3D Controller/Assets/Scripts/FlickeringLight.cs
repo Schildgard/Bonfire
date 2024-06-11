3ddb1b25aa585ba3b947e58eb144ea6e4cd540f1
Schildgard <schildgard@gmail.com>
2024-06-11 09:38:27 +0200

    Flickering Light
    Implemented flickering Light for torches

---
+++ 3D Controller/Assets/Scripts/FlickeringLight.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlickeringLight : MonoBehaviour
+{
+
+    [SerializeField] private Light Light;
+
+
+    private float timer;
+    private float startLightIntensity;
+   [SerializeField]private float flickerIntensity;
+   [SerializeField]private float flickerPerSecond;
+   [SerializeField]private float speedRandomness;
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Light = GetComponent<Light>();
+        startLightIntensity = Light.intensity;
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime * (1 - Random.Range(-speedRandomness, speedRandomness)) * Mathf.PI;
+        Light.intensity = startLightIntensity = startLightIntensity + Mathf.Sin(timer * flickerPerSecond) * flickerIntensity;
+
+    }
+}
