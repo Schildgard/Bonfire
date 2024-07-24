668da919b575b8e9b9992ff95beef806b0815e1a
Kate <schildgard@gmail.com>
2024-07-24 18:42:10 +0200

    MeshGenerationChanges

--- 3D Controller/Assets/Scripts/Mesh Generation/Noise/NoiseSettings.cs
+++ 3D Controller/Assets/Scripts/Mesh Generation/Noise/NoiseSettings.cs
@@ -9,11 +9,11 @@ public class NoiseSettings
     [Range(1, 8)]
     public int LayerCount; //Noise Position gets evaluated for each layer which makes the more detailed
 
-    public float BaseRoughness; //Smoothens the Noise
+    public float BaseRoughness; //Zieht das Noise zusammen/auseinander  // Zoom in das Noise
     public float Roughness;
 
     public float Strength; // increases the Noise frequency
-    public float Persistence;
+    public float Persistence; // Mehr / Weniger Noise
 
     public Vector3 NoiseCenter;
 
